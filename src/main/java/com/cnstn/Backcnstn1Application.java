bd05d69de3be07af3738ef0e84224792ebdb1e6c
aymenghrissi <ghrissiaymen64@gmail.com>
2023-05-12 03:49:49 -0500

    f

--- src/main/java/com/cnstn/Backcnstn1Application.java
+++ src/main/java/com/cnstn/Backcnstn1Application.java
@@ -6,10 +6,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @SpringBootApplication
 public class Backcnstn1Application {
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*")
+				.allowedMethods("GET","PUT","POST","DELETE","OPTIONS")
+				.allowedHeaders("*")
+				.allowedOriginPatterns("*")
+				.allowCredentials(true).maxAge(3600);
+				
+			}
+		};
+	}
+
 	@Bean	
 	 public PasswordEncoder passwordEncoder() {
 		 return new BCryptPasswordEncoder();
